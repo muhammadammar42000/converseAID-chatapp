844d163789ec461c0fdfe9c88b8e6e319ab3eda0
Bilal Irfan <bilalirfan000@gmail.com>
2024-09-17 01:06:48 +0500

    set notify

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -7,58 +7,64 @@ import CustomerSupport from "../../../../public/conserse.png";
 import GoogleLogo from "../../../../public/google.png";
 import MicrosoftLogo from "../../../../public/microsoft.png";
 import Link from "next/link";
-import { fetchSignInMethodsForEmail, getAuth, GoogleAuthProvider, OAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
-import * as Yup from 'yup'
-import { useFormik } from 'formik'
+import {
+  fetchSignInMethodsForEmail,
+  getAuth,
+  GoogleAuthProvider,
+  OAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import useStore from "@/lib/zustand";
 import { shallow } from "zustand/shallow";
-import { auth, dataBase, } from "@/firebase/firebase";
+import { auth, dataBase } from "@/firebase/firebase";
 import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { useToast } from "@chakra-ui/react";
 
 const LoginPage = () => {
-  const toast = useToast()
+  const toast = useToast();
 
-  const [loading, setLoading] = useState(false)
-  const [googleLoading, setGoogleLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [googleLoading, setGoogleLoading] = useState(false);
 
   const { setUser, setNotify } = useStore(
     (state) => ({
       setUser: state.setUser,
-      setNotify: state.setNotify
+      setNotify: state.setNotify,
     }),
     shallow
-  )
-  const handleSubmit = async (values,) => {
-    const { email, password } = values
-    setLoading(true)
+  );
+  const handleSubmit = async (values) => {
+    const { email, password } = values;
+    setLoading(true);
     signInWithEmailAndPassword(auth, email, password)
       .then(async (userCredential) => {
         // Signed in
-        const user = userCredential.user
-        const usersRef = collection(dataBase, 'users')
+        const user = userCredential.user;
+        const usersRef = collection(dataBase, "users");
 
-        const q = query(usersRef, where('userId', '==', user.uid))
+        const q = query(usersRef, where("userId", "==", user.uid));
 
         // Adjust "users" to your specific collection name
-        const querySnapshot = await getDocs(q)
+        const querySnapshot = await getDocs(q);
         if (!querySnapshot.empty) {
-          console.log('dddd')
-          setLoading(false)
-          const userDataOne = querySnapshot.docs[0].data()
+          setLoading(false);
+          const userDataOne = querySnapshot.docs[0].data();
           // Combine user auth data with Firestore data
-          console.log(userDataOne,'----')
-          const userData = { uid: user.uid, email: user.email, ...userDataOne }
-          console.log(userData)
-          setUser(userData) // Assuming you have a setUser function to set user state
+          console.log(userDataOne, "----");
+          const userData = { uid: user.uid, email: user.email, ...userDataOne };
+          console.log(userData);
+          setUser(userData); // Assuming you have a setUser function to set user state
           // setSnackbarMessage('Sign in successful!')
           // setSnackbarSeverity('success')
           // setOpenSnackbar(true)
         } else {
-          setLoading(false)
+          setLoading(false);
 
           // Handle case where user data does not exist in Firestore
-          console.error('No user data found in Firestore')
+          console.error("No user data found in Firestore");
           // setSnackbarMessage('No user data found.')
           // setSnackbarSeverity('error')
           // setOpenSnackbar(true)
@@ -66,97 +72,102 @@ const LoginPage = () => {
         // You can redirect the user to another page or update the UI accordingly
       })
       .catch((error) => {
-        const errorCode = error.code
-        const errorMessage = error.message
+        const errorCode = error.code;
+        const errorMessage = error.message;
         // setErrors({ email: errorMessage })
         // setSnackbarMessage(errorMessage)
         // setSnackbarSeverity('error')
         // setOpenSnackbar(true)
         toast({
-          position: 'top-right',
-          status: 'error',
-          title: errorCode??'Invalid Credintial',
+          position: "top-right",
+          status: "error",
+          title: errorCode ?? "Invalid Credential",
           duration: 9000,
-          isClosable: true
-        })
-        setLoading(false)
+          isClosable: true,
+        });
+        setLoading(false);
 
-        console.error(errorCode, errorMessage)
-      })
-  }
+        console.error(errorCode, errorMessage);
+      });
+  };
 
   const microsoftLogin = async () => {
     const auth = getAuth();
-    const provider = new OAuthProvider('microsoft.com');
-  
+    const provider = new OAuthProvider("microsoft.com");
+
     try {
       const result = await signInWithPopup(auth, provider);
-      
+
       // This gives you a Microsoft Access Token.
       const credential = OAuthProvider.credentialFromResult(result);
       const accessToken = credential.accessToken;
-      
+
       // The signed-in user info.
       const user = result.user;
-  
-      console.log('User Info:', user);
-      console.log('Access Token:', accessToken);
+
+      console.log("User Info:", user);
+      console.log("Access Token:", accessToken);
     } catch (error) {
-      console.error('Error during Microsoft login:', error);
+      setNotify({
+        open: true,
+        type: "error",
+        message: `Error during Microsoft login`,
+      });
+     
+      console.error("Error during Microsoft login:", error);
     }
   };
-  
 
   const initialValues = {
     email: null,
-    password: null
-  }
+    password: null,
+  };
   const validationSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Required'),
-    password: Yup.string().min(6).required('Required'),
-  })
-
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string().min(6).required("Required"),
+  });
 
   const formik = useFormik({
     initialValues: initialValues,
     validationSchema: validationSchema,
     onSubmit: async (values) => {
       // Handle form submission, e.g., send data to a backend API
-      handleSubmit(values)
-    }
-  })
+      handleSubmit(values);
+    },
+  });
 
   const googleLogin = () => {
-    setGoogleLoading(true)
+    setGoogleLoading(true);
     const auth = getAuth();
-   
-    const provider = new GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/user.birthday.read');
-    provider.addScope('https://www.googleapis.com/auth/user.gender.read');
-     
-   
-    signInWithPopup(auth,provider)
-      .then(async (result) => {
-        const usersRef = collection(dataBase, 'users')
-        const q = query(usersRef, where('userId', '==', result?.user?.uid))
-        // Adjust "users" to your specific collection name
-        const querySnapshot = await getDocs(q)
-        if (!querySnapshot.empty) {
-          setGoogleLoading(false)
-          const userDataOne = querySnapshot.docs[0].data()
-          // Combine user auth data with Firestore data
-          const userData = { uid: userDataOne?.userId, email: userDataOne.email, ...userDataOne }
-          setUser(userData) // Assuming you have a setUser function to set user state
-          toast({
-            position: 'top-right',
-            status: 'success',
-            title: 'Login Successfully',
-            duration: 9000,
-            isClosable: true
-          })
 
-        }
-        else {
+    const provider = new GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/user.birthday.read");
+    provider.addScope("https://www.googleapis.com/auth/user.gender.read");
+
+    signInWithPopup(auth, provider)
+      .then(async (result) => {
+        const usersRef = collection(dataBase, "users");
+        const q = query(usersRef, where("userId", "==", result?.user?.uid));
+        // Adjust "users" to your specific collection name
+        const querySnapshot = await getDocs(q);
+        if (!querySnapshot.empty) {
+          setGoogleLoading(false);
+          const userDataOne = querySnapshot.docs[0].data();
+          // Combine user auth data with Firestore data
+          const userData = {
+            uid: userDataOne?.userId,
+            email: userDataOne.email,
+            ...userDataOne,
+          };
+          setUser(userData); // Assuming you have a setUser function to set user state
+          toast({
+            position: "top-right",
+            status: "success",
+            title: "Login Successfully",
+            duration: 9000,
+            isClosable: true,
+          });
+        } else {
           const userData = {
             active: true,
             createAt: new Date(),
@@ -164,51 +175,53 @@ const LoginPage = () => {
             email: result?.user?.email,
             name: result?.user?.displayName,
             userId: result?.user?.uid,
-          }
+          };
           try {
-            
-            addDoc(collection(dataBase, 'users'), userData).then(() => {
-              setGoogleLoading(false)
-            
-              setUser(userData) // Assuming you have a setUser function to set user state'
+            addDoc(collection(dataBase, "users"), userData).then(() => {
+              setGoogleLoading(false);
+
+              setUser(userData); // Assuming you have a setUser function to set user state'
               toast({
-                position: 'top-right',
-                status: 'success',
-                title: 'Login Successfully',
+                position: "top-right",
+                status: "success",
+                title: "Login Successfully",
                 duration: 9000,
-                isClosable: true
-              })
-            })
+                isClosable: true,
+              });
+            });
           } catch (e) {
-            setLoading(false)
-            console.error('Error adding document: ', e)
+            setLoading(false);
+            console.error("Error adding document: ", e);
           }
         }
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
-
-      }).catch(async (error) => {
+      })
+      .catch(async (error) => {
         setGoogleLoading(false);
-  
-        if (error.code === 'auth/account-exists-with-different-credential') {
+
+        if (error.code === "auth/account-exists-with-different-credential") {
           // The email is already associated with another provider
           const email = error.customData.email;
           const methods = await fetchSignInMethodsForEmail(auth, email);
-  
+
           if (methods.length > 0) {
-            alert(`This email is already associated with ${methods[0]}. Please use that method to log in.`);
+            alert(
+              `This email is already associated with ${methods[0]}. Please use that method to log in.`
+            );
           } else {
-            alert('This email is already associated with another account. Please try a different login method.');
+            alert(
+              "This email is already associated with another account. Please try a different login method."
+            );
           }
         } else {
           // Handle other errors
-          console.error('Error during sign-in:', error);
+          console.error("Error during sign-in:", error);
         }
-      })
-  }
+      });
+  };
 
   return (
-
     <div className="grid grid-cols-1 md:grid-cols-2 bg-white h-[100vh]">
       {/* Left Column */}
       <div className="hidden md:flex flex-col bg-gradient-to-b from-white to-blue-700">
@@ -216,13 +229,16 @@ const LoginPage = () => {
           <Image src={logo} className="w-[300px]" alt="logo" />
         </div>
         <div className="flex justify-center items-center h-full">
-          <Image src={CustomerSupport} className="max-w-full" alt="CustomerSupport" />
+          <Image
+            src={CustomerSupport}
+            className="max-w-full"
+            alt="CustomerSupport"
+          />
         </div>
       </div>
       {/* Right Column */}
       <div className="p-20 flex flex-col justify-center items-center">
-        <form className="px-20"
-          onSubmit={formik.handleSubmit}>
+        <form className="px-20" onSubmit={formik.handleSubmit}>
           <div className="text-left mb-6">
             <h2 className="text-2xl font-bold mb-2">Account Login</h2>
             <p className="text-gray-500">
@@ -261,7 +277,6 @@ const LoginPage = () => {
                 name="password"
                 className="appearance-none border rounded w-full py-3 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 onChange={formik.handleChange}
-
               />
             </div>
             <div className="mb-6">
@@ -279,16 +294,31 @@ const LoginPage = () => {
                 disabled={loading}
                 className="bg-blue-500 hover:bg-blue-700 text-white py-3 px-4 rounded focus:outline-none focus:shadow-outline w-full"
               >
-                {loading ? <div class="text-center">
-                  <div role="status">
-                    <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-white-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                    </svg>
-                    <span class="sr-only">Loading...</span>
+                {loading ? (
+                  <div class="text-center">
+                    <div role="status">
+                      <svg
+                        aria-hidden="true"
+                        class="inline w-8 h-8 text-gray-200 animate-spin dark:text-white-600 fill-blue-600"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="currentColor"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentFill"
+                        />
+                      </svg>
+                      <span class="sr-only">Loading...</span>
+                    </div>
                   </div>
-                </div> : <p>Login</p>}
-
+                ) : (
+                  <p>Login</p>
+                )}
               </button>
             </div>
             <div className="mt-6 text-center">
@@ -313,13 +343,16 @@ const LoginPage = () => {
                 onClick={() => googleLogin()}
                 className="w-full flex items-center  border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:shadow-outline mb-2"
               >
-                <Image src={GoogleLogo} className="max-w-full w-[30px] mr-2"  alt="GoogleLogo"/>
+                <Image
+                  src={GoogleLogo}
+                  className="max-w-full w-[30px] mr-2"
+                  alt="GoogleLogo"
+                />
                 Continue with Google
               </button>
               <button
                 type="button"
                 onClick={() => microsoftLogin()}
-
                 className="w-full flex items-center border border-gray-300 rounded-lg py-3 px-4 focus:outline-none focus:shadow-outline"
               >
                 <Image
